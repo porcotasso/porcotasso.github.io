dfb2c4d4081f118e2cf5f4455469175becb0828b
Porco Kafuka <>
2020-06-09 23:05:55 +0800

    add gulp-postscss, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+
+const autoprefixerOption = {
+    grid: true
+}
+const postcssOption =[ autoprefixer(autoprefixerOption)]
 
 gulp.task('sass', () => {
-
-   return gulp.src('./scss/bootstrap.scss')
-   .pipe(sass())
-   .pipe(gulp.dest('./css'))
- 
-}
+    return gulp.src('./scss/bootstrap/bootstrap.scss')
+    .pipe(sass())
+    .pipe(postcss(postcssOption))
+    .pipe(gulp.dest('./css'))
+    }
 )
