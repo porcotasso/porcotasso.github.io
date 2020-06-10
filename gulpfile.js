19ccedc52eb8da6ba5d545bc2d01bcde3af94f2b
Porco Kafuka <>
2020-06-10 09:09:38 +0800

    add csswring, flexbugs-fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,27 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+const flexBugsFixers = require('postcss-flexbugs-fixes')
+const cssWring = require('csswring')
 
 const autoprefixerOption = {
     grid: true
 }
 
-const postcssOption =[ autoprefixer(autoprefixerOption) ]
+const postcssOption = [
+    flexBugsFixers,
+    autoprefixer(autoprefixerOption)
+    // cssWring
+]
 
 gulp.task('sass', () => {
-    return gulp.src('./scss/bootstrap/bootstrap.scss')
+    return gulp.src('./scss/common.scss')
+    // return gulp.src('./scss/bootstrap/bootstrap.scss')
     .pipe(sass())
     .pipe(postcss(postcssOption))
     .pipe(gulp.dest('./css'))
-    }
-)
+})
+
+gulp.task('watch', () => {
+    return gulp.watch('./scss/**/*.scss', gulp.series('sass'))
+})
