49afbff8c8c4b615310b478ad22207e2a915a137
Porco Kafuka <>
2021-01-01 18:59:36 +0800

    edit layout

--- js/home.js
+++ js/home.js
@@ -26,36 +26,13 @@ var mySwiper = new Swiper ('.swiper-container', {
 });
 
 
-//home
+//home summary logo
 const logoMark = document.getElementById('js-logoMark');
-
 function showElementAnimation() {
-	console.log('---');
-	// let logoMarkLeft = logoMark.style.left;
-	// let logoMarkLeftNum = parseFloat(logoMarkLeft);
-	// var showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
-	var showTiming = 0;
-	var scrollY = window.pageYOffset;
-	// if(scrollY > 100){
-		logoMark.style.left = scrollY - 100 + scrollY*scrollY/1000;
-		logoMark.style.top = scrollY/3 - 80;
-		logoMark.style.opacity = 0.3 - scrollY/1200;
-	// }
-	console.log(scrollY);
-	// var windowH = window.innerHeight;
-	// var elemClientRect = logoMark.getBoundingClientRect(); 
-	// var elemY = scrollY + elemClientRect.top; 
-	// console.log('scrollY');
-	// console.log(scrollY);
-	// console.log('elemClientRect');
-	// console.log(elemClientRect);
-	// if(scrollY + windowH - showTiming > elemY) {
-	//   logoMark.style.left = scrollY -100;
-	//   // logoMark.style.top = 100 + scrollY;
-	// } else if(scrollY + windowH < elemY) {
-	//   logoMark.style.left = -scrollY - 100;
-	//   // logoMark.style.top = 100 - scrollY;
-	// }
-  }
-  showElementAnimation();
-  window.addEventListener('scroll', showElementAnimation);
+var scrollY = window.pageYOffset;
+logoMark.style.left = scrollY/2 - 100;
+logoMark.style.top = scrollY/4 - 60;
+logoMark.style.opacity = 0.3 - scrollY/1200;
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
