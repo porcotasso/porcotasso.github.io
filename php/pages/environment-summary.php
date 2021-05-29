95680dcccbd382b02db80d1587a88c5c42cab711
Porco Kafuka <>
2021-05-29 23:18:38 +0900

    edit layout

--- php/pages/environment-summary.php
+++ php/pages/environment-summary.php
@@ -1,7 +1,7 @@
 <?php include("../_partial/wrapper-head.php"); ?>
 <main> 
     <article class="articleCnt">
-		<?php $navList = array("何をしたいかを明確にする", "管理と構成と流れ", "homebrewをインストール", "homebrewでgitをインストール", "anyenvをインストール", "nodenvをインストール", "sassをcssに"); ?>
+		<?php $navList = array("何をしたいかを明確にする", "管理と構成と流れ", "homebrewをインストール", "homebrewでgitをインストール", "gitでanyenvをインストール", "anyenvでnodenvをインストール", "nodeenvでnode.jsをインストール", "Node.jsでgulpをインストール","homebrewでfishをインストール","omebrewでphpをインストール","homebrewでnginxをインストール","homebrewでmariadbをインストール"); ?>
         <?php include("../_partial/pages-title.php"); ?>
             
                 
@@ -15,7 +15,7 @@
 			<p>htmlで全部書くとか無理。php使って楽したい。</p>
 			<p>phpを使うには、homebrewを使ってインストールするのがいい。</p>
 			<h3>ローカルサーバでバーチャルホストをたてる</h3>
-			<p>MAMPは使わない。nginxを使う。homebrewを使ってインストールするのがいい。ただhomebrewでのnginxは推奨はしない的なメッセージがでてくることがあるけど。</p>
+			<p>MAMPは使わない。nginxを使う。homebrewを使ってインストールするのがいい。ただhomebrewでのnginxは推奨はしない的なメッセージがでてくることがあるけど、問題が出るまでスルー。</p>
 			<h3>コマンド打ちを楽したい</h3>
 			<p>fishを入れる。</p>
 		</section>
@@ -26,16 +26,16 @@
 				<li>なにはともあれvisual Studio Codeをインストール</li>
 				<li>Homebrewをインストール</li>
 				<li>homebrewでgitをインストール</li>
-				<li>homebrewでfishをインストール</li>
-				<li>homebrewでphpをインストール</li>
-				<li>homebrewでnginxをインストール</li>
-				<li>homebrewでmariadbをインストール</li>
 				<li>gitでanyenvをインストール</li>
 				<li>anyenvでnodeenvをインストール</li>
 				<li>nodeenvでNode.jsをインストール</li>
 				<li>Node.jsでgulpをインストール</li>
-				<li>Node.jsでwebpackをインストール</li>
-				<li>Node.jsでtypescriptをインストール</li>
+				<li>homebrewでfishをインストール</li>
+				<li>homebrewでphpをインストール</li>
+				<li>homebrewでnginxをインストール</li>
+				<li>homebrewでmariadbをインストール</li>
+				<!-- <li>Node.jsでwebpackをインストール</li>
+				<li>Node.jsでtypescriptをインストール</li> -->
 			</ol>
 
 			<h3>anyenvでインストールするもの</h3>
@@ -61,11 +61,11 @@
 <pre class="prettyprint">
 brew install git
 </pre>
-もしgitがhomebrew以外の方法で既にインストールされている場合、 そっちを起動する可能性がある。
+もしgitがhomebrew以外の方法で既にインストールされている場合、 そっちを起動する可能性があるのでチェック。
 <pre class="prettyprint">
 which git
 </pre>
-<p>で場所がわかる。現在、/usr/local/bin/gitが使われるがこれはこちらのエイリアス。ー＞/usr/local/Cellar/git/2.26.2/bin/git実際のデータはこちらにある。</p>
+<p>で場所わかる。現在、/usr/local/bin/gitが使われるがこれはこちらのエイリアス。<br>→/usr/local/Cellar/git/2.26.2/bin/git実際のデータはこちらにある。</p>
 		</section>
         <section class="ly-section tocContent">
 		<h2><?php echo $navList[4]; ?></h2>
@@ -77,9 +77,14 @@ which git
 //ユーザーディレクトリ以下に隠しファイル'.anyenv'という名前でanyenvをgithubからクローン.
 git clone https://github.com/anyenv/anyenv ~/.anyenv
 //fish用にパスの追加
-set -U fish-user-paths ~/.anyenv/bin $fish-user-paths
+set -U fish-user-paths $HOME/.anyenv/bin $fish-user-paths
+*注意！~を使わずに必ず$HOMEを使う
+x -> set -U fish-user-paths ~/.anyenv/bin $fish-user-paths
 //初期化
 anyenv install --init
+
+Do you want to checkout？ [y/N];
+y　->　次のコマンドanyenv install --list が使えるようになる
 </pre>
         </section>
 		<section class="ly-section tocContent">
@@ -92,12 +97,20 @@ anyenv install --init
 //インストールできる**envのリストが表示されます。
 anyenv install --list
 
-//エラー'no such command install'が出る場合、下記に移動してsh実行でinstallコマンドが入る
-cd ~/.anyenv/envs/nodenv/plugins/node-build 
-sh install.sh
-
+//nodeenvのインストール
+anyenv install nodenv
+</pre>
+		</section>
+		<section class="ly-section tocContent">
+			<h2><?php echo $navList[6]; ?></h2>
+<pre class="prettyprint">
 //nodenv のインストール　~/.nodenv/versions/12.16.3 ローカルに入る
 nodenv install 12.16.3
+
+//エラー'no such command install'が出る場合、下記に移動してsh実行でinstallコマンドが入る
+cd ~/.anyenv/envs/nodenv/plugins/node-build 
+sh install.sh 　実行
+
 
 //12.16.3
 nodenv versions
@@ -120,55 +133,71 @@ nodenv versions
 //その他コマンドを表示
 nodenv
 </pre>
+<cite class="ly-cite"><a class="ly-cite_link" href="https://qiita.com/SHIN_DEVELOP/items/5d5687465ee6a64320df" <?php echo $targetBlank ?>>anyenvからnodenv入れるときにややハマりした話</a></cite>
+
         <p>ちなみに以前nodebrewを入れていたので削除。パスの削除もすること。</p>
 <pre class="prettyprint">
 brew uninstall nodebrew
 </pre>
 		</section>
 		<section class="ly-section tocContent">
-		<h2><?php echo $navList[6]; ?></h2>
+		<h2><?php echo $navList[7]; ?></h2>
 		<ol>
-			<li>作業directoryの決定（これからこの下にデータを作っていくってフォルダを作ります httpdocs下とか）</li>
+			<li>作業directoryの決定（これからこの下にデータを作っていくってフォルダを作ります。projectsとか）</li>
 			<li>visual code studio 開く。</li>
 			<li>File-> new window でそのフォルダ選択して開く。terminal のdirectly がそのフォルダに移動される。</li>
 			<li>terminalで Node.jsをインストール（すでにしてたら無視）</li>
-			<li>npm initで作業場所の宣言&package.json作成</li>
-			<li>npm install でGulpのインストール</li>
-			<li>gulpfile.js の作成　</li>
-			<li>package.json、gulpfile.js編集</li>
-			<li>npm run で実行</li>
-		</ol>
-		<h3>作業ディレクトリの決定</h3>
-		<p>npm initで「このディレクトリ下は 我々npmが 管理下に置く！」宣言。ついでにpackage.jsonファイルも作成される。package.jsonにはプロジェクトの設定情報が書いてあります。Nodeからどんな便利な機能を追加するなど。</p>
+			<li><p>npm initで作業ディレクトリの宣言&package.json作成</p>
+				<p>npm initで「このディレクトリ下は 我々npmが 管理下に置く！」宣言。ついでにpackage.jsonファイルも作成される。package.jsonにはプロジェクトの設定情報が書いてあります。Nodeからどんな便利な機能を追加するなど。</p>
 <pre class="prettyprint">
 npm init -y
 </pre>
-		<h3>Gulpのインストール　方法１</h3>
-		<p>package.jsonのdevDependenciesにインストールしたプラグイン名とバージョンが記載されます。node_modulesというフォルダ下に、ライブラリが保存されます。 (node_modulesフォルダが作業場所に作成される)</p>
+</li>
+			<li>npm install でGulpのインストール
+				<h4>Gulpのインストール　方法１</h4>
+				<p>package.jsonのdevDependenciesにインストールしたプラグイン名とバージョンが記載されます。node_modulesというフォルダ下に、ライブラリが保存されます。 (node_modulesフォルダが作業場所に作成される)</p>
 <pre class="prettyprint">
 npm install -D gulp
 </pre>
-		<h3>Gulpのインストール　方法２</h3>
-		<p>package.jsonのdependencies内に入ったライブラリは、npm installだけで自動的にインストールすることができます。（他の同じ環境の制作者とpackage.jsonを統一するときとか、package.jsonをもらってnpm installすれば一気にDLできる）</p>
-		<h3>gulpfile.js の作成</h3>
-		<p>package.json、gulpfile.js編集（色々参照してコピって編集）重要だけど長くなるのでここは省く</p>
+				<h4>Gulpのインストール　方法２</h4>
+				<p>package.jsonのdependencies内に入ったライブラリは、npm installだけで自動的にインストールすることができます。（他の同じ環境の制作者とpackage.jsonを統一するときとか、package.jsonをもらってnpm installすれば一気にDLできる）</p>			
+			</li>
+			<li>gulpfile.js の作成　</li>
+			<li>package.json、gulpfile.js編集</li>
+			<li>npm run で実行</li>
 <pre class="prettyprint">
 実行
 
 $ npm run dev
 npm run dev -> package.json の中身をみる　→　
 
- "scripts": {
-   "dev": "gulp"
- }
+"scripts": {
+"dev": "gulp"
+}
 -> gulpfile.js の
 
 gulp.task( {
 });
 が実行される。
 </pre>
+		</ol>
+		
+		
 		<cite class="ly-cite"><a class="ly-cite_link" href="https://ics.media/entry/3290/" <?php echo $targetBlank ?>>絶対つまずかないGulp 4入門</a></cite>
 		<cite class="ly-cite"><a class="ly-cite_link" href="https://qiita.com/hashrock/items/15f4a4961183cfbb2658" <?php echo $targetBlank ?>>フロントエンド開発の３ステップ（npmことはじめ）</a></cite>
+		</section>
+		<section class="ly-section tocContent">
+			<h2><?php echo $navList[8]; ?></h2>
+
+		</section>
+		<section class="ly-section tocContent">
+			<h2><?php echo $navList[9]; ?></h2>
+		</section>
+		<section class="ly-section tocContent">
+			<h2><?php echo $navList[10]; ?></h2>
+		</section>
+		<section class="ly-section tocContent">
+			<h2><?php echo $navList[11]; ?></h2>
 		</section>
 
     </article>
