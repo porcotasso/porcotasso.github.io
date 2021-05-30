4daeceae3f88a808aeade920643fa82d2f9fb273
Porco Kafuka <>
2021-05-30 23:31:09 +0900

    edit layout

--- php/index.php
+++ php/index.php
@@ -1,12 +1,13 @@
 <?php include("_partial/wrapper-head.php"); ?>
 <main>
     <div class="bl-summary ly-innnerWidth">
-        <h2 class="bl-summary_ttl">ウェブを楽しむ</h2>
+        <div class="content-list">
+            <a href="/frontend.html">
+                <h2 class="bl-summary_ttl">frontend skill</h2>
+            </a>
+        </div>
         <div class="bl-summary_txt">
-            <p>フロントエンドエンジニア、ポルコ•タッソのこつこつ技術メモです。 </p>
-            <p>知らないことは理解するのを楽しもう<br>できないことは身につくのを楽しもう</p>
-            <p>まとまってないメモはそのうち清書予定。多分。</p>
-            <a href="/frontend.html">frontend</a>
+ 
             <a href="/profile.html">profile.html</a>
         </div>
     </div>
