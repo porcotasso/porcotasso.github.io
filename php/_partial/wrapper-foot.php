be5b5a242966bf925d64202ddfbeecca2d680b15
Porco Kafuka <>
2021-05-31 23:46:46 +0900

    edit layout

--- php/_partial/wrapper-foot.php
+++ php/_partial/wrapper-foot.php
@@ -1,5 +1,5 @@
         <footer class="bl-footer">
-            <p class="el-copyright">&copy; 2019-<?php echo date('Y') ?> porco tasso</p>
+            <p class="el-copyright">&copy; 2019-<?php echo date('Y') ?> Porconote</p>
         </footer>
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
         <script src="/js/common.js"></script>
