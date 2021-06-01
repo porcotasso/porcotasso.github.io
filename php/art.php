e2441eb68b332b4b065eda2c40a166e0bd4114c0
Porco Kafuka <>
2021-06-02 00:15:49 +0900

    edit layout

---
+++ php/art.php
@@ -0,0 +1,14 @@
+<?php include("_partial/wrapper-head.php"); ?>
+<main>
+
+       <img src="/images/illustration/illustration002.jpg">
+
+</main>
+
+
+
+
+
+    
+<script src="/js/home.js"></script>
+<?php include("_partial/wrapper-foot.php"); ?>
--- php/index.php
+++ php/index.php
@@ -1,11 +1,10 @@
 <?php include("_partial/wrapper-head.php"); ?>
 <main>
-    <!-- <img src="/images/illustration/illustration002.jpg"> -->
 
         
     <ul class="bl-menu">
-        <li class="bl-menu_list"><a class="bl-menu_link" href="/profile.html">art</a></li>
-        <li class="bl-menu_list"><a class="bl-menu_link" href="/profile.html">profile</a></li>
+        <li class="bl-menu_list"><a class="bl-menu_link" href="art.html">art</a></li>
+        <li class="bl-menu_list"><a class="bl-menu_link" href="profile.html">profile</a></li>
     </ul>
 </main>
 <script src="/js/home.js"></script>
