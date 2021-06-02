813d35e6bbc1db19a6a888cc994ffd976f3f9a4b
Porco Kafuka <>
2021-06-02 21:46:39 +0900

    edit layout

--- php/_partial/wrapper-head.php
+++ php/_partial/wrapper-head.php
@@ -116,7 +116,15 @@
     </header>
     <nav class="headerNav">
         <div class="headerMenu" id="js-headerMenu">
-            <?php foreach($pageLists as $list){ ?>
+
+        <div class="headerMenu_detail">
+            <p class="headerMenu_ttl"><a class="headerMenu_link" href="frontend.html">codenote</a></p>
+            <p class="headerMenu_ttl"><a class="headerMenu_link" href="art.html">artnote</a></p>
+            <p class="headerMenu_ttl"><a class="headerMenu_link" href="profile.html">profile</a></p>
+        </div>
+
+
+            <?php /* foreach($pageLists as $list){ ?>
             <div class="headerMenu_detail">
                 <p class="headerMenu_ttl js-headerMenuTtl"><?php echo $list["title"]; ?><span class="summary_cntNumber"><?php echo count($list["pages"]) ?></span></p>
                 <ul class="headerMenu_ul">
@@ -129,7 +137,7 @@
                     <?php } ?>
                 </ul>
             </div>
-            <?php } ?>
+            <?php } */ ?>
         </div>
     </nav>
 
--- php/index.php
+++ php/index.php
@@ -2,10 +2,10 @@
 <main>
 
         
-    <ul class="bl-menu">
+    <!-- <ul class="bl-menu">
         <li class="bl-menu_list"><a class="bl-menu_link" href="art.html">art</a></li>
         <li class="bl-menu_list"><a class="bl-menu_link" href="profile.html">profile</a></li>
-    </ul>
+    </ul> -->
 </main>
 <script src="/js/home.js"></script>
 <?php include("_partial/wrapper-foot.php"); ?>
